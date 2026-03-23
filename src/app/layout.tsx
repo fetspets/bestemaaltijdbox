0dec513e638ae87e0e9a6d63559052902132aaef
Stef <stefmarien@gmail.com>
2026-03-23 20:27:20 +0100

    TradeTracker verificatie toegevoegd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,22 +20,25 @@ export const metadata: Metadata = {
   },
   robots: { index: true, follow: true },
   alternates: { canonical: 'https://www.bestemaaltijdbox.be' },
-  verification: {
-    google: 'dgD6dFbp4JHPwAeuIu27zU6WTq1wEII8Sj_RsjngfgA',
-  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="nl">
       <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           href="https://fonts.googleapis.com/css2?family=Fraunces:ital,opsz,wght@0,9..144,700;0,9..144,900;1,9..144,700&family=DM+Sans:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
-        {/* Google Analytics */}
+      </head>
+      <body>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+         {/* Google Analytics */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-MPESC3XFFN" />
         <script dangerouslySetInnerHTML={{ __html: `
           window.dataLayer = window.dataLayer || [];
@@ -43,11 +46,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           gtag('js', new Date());
           gtag('config', 'G-MPESC3XFFN');
         `}} />
-      </head>
-      <body>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
+        <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `
+          var _TradeTrackerTagOptions = {
+            t: 'a',
+            s: '507243',
+            chk: '13932be1a3acb66bb8b510c670298bb4',
+            overrideOptions: {}
+          };
+          (function() {var tt = document.createElement('script'), s = document.getElementsByTagName('script')[0]; tt.setAttribute('type', 'text/javascript'); tt.setAttribute('src', (document.location.protocol == 'https:' ? 'https' : 'http') + '://tm.tradetracker.net/tag?t=' + _TradeTrackerTagOptions.t + '&s=' + _TradeTrackerTagOptions.s + '&chk=' + _TradeTrackerTagOptions.chk); s.parentNode.insertBefore(tt, s);})();
+        `}} />
       </body>
     </html>
   );
