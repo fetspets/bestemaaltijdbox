ed88f601447a600c9887236b143936373e033385
Stef <stefmarien@gmail.com>
2026-03-29 12:22:03 +0200

    smartmat verwijderd uit rankings, vermelding bij foodbag

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/aanbieder/smartmat',
-        destination: '/aanbieder/foodbag',
-        permanent: true,
-      },
-      {
-        source: '/aanbieder/15gram',
-        destination: '/aanbieder/foodbag',
-        permanent: true,
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
